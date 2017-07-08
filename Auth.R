838acbc205485179206a2376f7788efc462e62e3
Amol Tangade <amolmt@users.noreply.github.com>
2017-07-09 00:45:58 +0530

    Update Auth.R

--- Auth.R
+++ Auth.R
@@ -1,12 +1,12 @@
 library(devtools)
 library(twitteR)
-api_key <- "4u0Opcx93z1Hy9hB67JwHT8Qk"
+api_key <- "your api key"
 
-api_secret <- "3hkCavPZCegtEM5zKLzxQWXD647hT4H2HAmf3nWQwvLAiq7omF"
+api_secret <- "api_secret key goes here"
 
-access_token <- "2460931303-MCTwWxafPnnIZ7bKOFIDBLlA08jUGwCMe9WMSQ3"
+access_token <- "access token goes here"
 
-access_token_secret <- "	cdIf3KOonuL8T3KG8BX1DcSYnI4daxRGqLzaadEYN9wWC"
+access_token_secret <- "	access token secret goes here"
 
 setup_twitter_oauth(api_key,api_secret)
 searchTwitter("iphone",n=150)
